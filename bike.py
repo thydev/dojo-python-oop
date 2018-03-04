db2a385c17b8ac5810dbad4df5876e9f78a4d70f
Chanthy Ngin <nginchanthy@gmail.com>
2018-03-04 09:23:04 -0800

    First object bike

---
+++ bike.py
@@ -0,0 +1,32 @@
+class Bike(object):
+    def __init__(self, price, max_speed):
+        self.price = price
+        self.max_speed = max_speed
+        self.miles = 0
+    def displayInfo(self):
+        print "Bike information: Price: {}, Max Speed: {}, Mileage: {}".format(self.price, self.max_speed, self.miles)
+        return self
+    def ride(self):
+        print "Riding"
+        self.miles += 10
+        return self
+    def reverse(self):
+        print "Reversing"
+        self.miles -= 5
+        if self.miles < 0:
+            self.miles = 0
+        return self
+
+bike1 = Bike(200, "20mph")
+bike1.displayInfo()
+bike2 = Bike(400, "30mph")
+bike2.displayInfo()
+bike3 = Bike(1500, "25mph")
+bike3.displayInfo()
+
+bike1.ride().ride().ride().reverse()
+bike2.ride().ride().reverse().reverse()
+bike3.reverse().reverse().reverse()
+bike1.displayInfo()
+bike2.displayInfo()
+bike3.displayInfo()
